7770de0319b5cd1dfebae46c08aaa7bfd744c1f9
Alain Armand <aarmand.inbox@gmail.com>
2020-10-16 12:02:14 +0200

    update dep/demo

--- demo/App.js
+++ demo/App.js
@@ -1,19 +1,36 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
  */
 
-import React, { Component } from 'react';
-import { Platform, Picker, ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 import Markdown, {
   AstRenderer,
   getUniqueID,
   PluginContainer,
   renderRules,
   styles,
-} from '../react-native-markdown-renderer';
-//
+} from 'react-native-markdown-renderer';
+
 import markdownItCheckbox from 'markdown-it-checkbox';
 import { TabViewAnimated, SceneMap, TabBar } from 'react-native-tab-view';
 
@@ -127,18 +144,15 @@ const initialLayout = {
   height: 0,
   width: Dimensions.get('window').width,
 };
-
-export default class App extends Component {
+const App: () => React$Node = () => {
   state = {
     index: 0,
     routes: [{ key: 'all', title: 'All' }, { key: 'linkedimg', title: 'Linked Images' }],
   };
-
   handleIndexChange = index => this.setState({ index });
   renderHeader = props => <TabBar {...props} />;
   renderScene = SceneMap(routes);
-
-  render() {
+  // render() {
     return (
       <TabViewAnimated
         navigationState={this.state}
@@ -148,5 +162,91 @@ export default class App extends Component {
         initialLayout={initialLayout}
       />
     );
-  }
-}
+  // };
+  // return (
+  //   <>
+  //     <StatusBar barStyle="dark-content" />
+  //     <SafeAreaView>
+  //       <ScrollView
+  //         contentInsetAdjustmentBehavior="automatic"
+  //         style={styles.scrollView}>
+  //         <Header />
+  //         {global.HermesInternal == null ? null : (
+  //           <View style={styles.engine}>
+  //             <Text style={styles.footer}>Engine: Hermes</Text>
+  //           </View>
+  //         )}
+  //         <View style={styles.body}>
+  //           <View style={styles.sectionContainer}>
+  //             <Text style={styles.sectionTitle}>Step One</Text>
+  //             <Text style={styles.sectionDescription}>
+  //               Edit <Text style={styles.highlight}>App.js</Text> to change this
+  //               screen and then come back to see your edits.
+  //             </Text>
+  //           </View>
+  //           <View style={styles.sectionContainer}>
+  //             <Text style={styles.sectionTitle}>See Your Changes</Text>
+  //             <Text style={styles.sectionDescription}>
+  //               <ReloadInstructions />
+  //             </Text>
+  //           </View>
+  //           <View style={styles.sectionContainer}>
+  //             <Text style={styles.sectionTitle}>Debug</Text>
+  //             <Text style={styles.sectionDescription}>
+  //               <DebugInstructions />
+  //             </Text>
+  //           </View>
+  //           <View style={styles.sectionContainer}>
+  //             <Text style={styles.sectionTitle}>Learn More</Text>
+  //             <Text style={styles.sectionDescription}>
+  //               Read the docs to discover what to do next:
+  //             </Text>
+  //           </View>
+  //           <LearnMoreLinks />
+  //         </View>
+  //       </ScrollView>
+  //     </SafeAreaView>
+  //   </>
+  // );
+};
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
+
+export default App;
